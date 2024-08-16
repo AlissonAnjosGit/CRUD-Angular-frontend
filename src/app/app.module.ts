9a856c5993be750b161b9df67525206a696108e4
Alisson Oliveira dos Anjos <alissonnewmail@gmail.com>
2024-08-16 17:34:40 -0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbar} from '@angular/material/toolbar'
+import {MatToolbar} from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
